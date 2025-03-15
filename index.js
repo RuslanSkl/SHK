db7c1802662b8f14ffcfba13b5466048ebb643f6
RuslanSkl <rsklyarenko@gmail.com>
2025-03-15 19:31:33 +0200

    - 'refactor'

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-document.querySelector(".menu-toggle").addEventListener("click", function() {
-    document.querySelector(".nav").classList.add("active");
-});
 
-document.querySelector(".close-btn").addEventListener("click", function() {
-    document.querySelector(".nav").classList.remove("active");
-});
